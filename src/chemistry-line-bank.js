55ab9ff255bd7486933673aa260344332794d3c6
ege-creator12 <mirzabalaevsaid78@gmail.com>
2026-09-07 18:59:33 +0300

    Assemble full chemistry EGE line bank

---
+++ src/chemistry-line-bank.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports={
+ ...require('./chemistry-bank-foundations'),
+ ...require('./chemistry-bank-inorganic'),
+ ...require('./chemistry-bank-organic'),
+ ...require('./chemistry-bank-processes'),
+ ...require('./chemistry-bank-calculations'),
+ ...require('./chemistry-bank-extended')
+};
