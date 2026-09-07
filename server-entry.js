32416366fd427ae801ffbb9c26204518e181c16c
ege-creator12 <mirzabalaevsaid78@gmail.com>
2026-09-07 16:43:54 +0300

    Add safe admin server entrypoint

---
+++ server-entry.js
@@ -0,0 +1,7 @@
+const database = require('./src/db');
+const originalRun = database.run;
+database.run = (sql, ...params) => originalRun(
+  sql.replace('updated_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP', 'updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP'),
+  ...params
+);
+require('./server-admin');
