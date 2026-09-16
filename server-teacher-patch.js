f5e430c429cb8c3a0220e19cd57f5abd8e27f6f4
ege-creator12 <mirzabalaevsaid78@gmail.com>
2026-09-16 20:34:14 +0300

    Route automatic homework grading before teacher v2

--- server-teacher-patch.js
+++ server-teacher-patch.js
@@ -3,6 +3,7 @@
 const moderator = require('./server-moderator');
 const teacher = require('./server-teacher');
 const teacherV2 = require('./server-teacher-v2');
+const teacherV3 = require('./server-teacher-v3');
 
 if (!moderator.__teacherConsolePatched) {
   const originalEnsureSchema = moderator.ensureSchema.bind(moderator);
@@ -11,9 +12,11 @@ if (!moderator.__teacherConsolePatched) {
   moderator.ensureSchema = async function ensureSchemaWithTeacher() {
     await originalEnsureSchema();
     await teacher.ensureSchema();
+    await teacherV3.ensureSchema();
   };
 
   moderator.handle = async function handleWithTeacher(req, res, url) {
+    if (await teacherV3.handle(req, res, url)) return true;
     if (await teacherV2.handle(req, res, url)) return true;
     if (await teacher.handle(req, res, url)) return true;
     return originalHandle(req, res, url);
