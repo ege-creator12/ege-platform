c2faa4fe681db292b4b03a312f140820f8cc26bd
ege-creator12 <mirzabalaevsaid78@gmail.com>
2026-09-16 17:53:59 +0300

    Use diversity overrides for chemistry line builders

--- src/chemistry-line-bank.js
+++ src/chemistry-line-bank.js
@@ -8,5 +8,6 @@ module.exports={
  ...require('./chemistry-bank-extended'),
  ...require('./chemistry-bank-foundations-ege'),
  ...require('./chemistry-bank-core-ege'),
- ...require('./chemistry-bank-first-part-ege')
+ ...require('./chemistry-bank-first-part-ege'),
+ ...require('./chemistry-bank-diverse-v3')
 };
