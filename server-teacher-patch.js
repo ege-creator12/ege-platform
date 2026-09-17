28ad2ce3abbe1cbeb66f4f27a23e123a64382769
ege-creator12 <mirzabalaevsaid78@gmail.com>
2026-09-17 21:10:08 +0300

    Route homework finalization through duplicate-call guard

--- server-teacher-patch.js
+++ server-teacher-patch.js
@@ -6,6 +6,7 @@ const teacherV2 = require('./server-teacher-v2');
 const teacherV3 = require('./server-teacher-v3');
 const teacherBatch = require('./server-teacher-batch');
 const teacherHomeworkRelaxed = require('./server-teacher-homework-relaxed');
+const teacherFinalizeGuard = require('./server-teacher-finalize-guard');
 const teacherFastRead = require('./server-teacher-fast-read');
 
 if (!moderator.__teacherConsolePatched) {
@@ -21,6 +22,7 @@ if (!moderator.__teacherConsolePatched) {
 
   moderator.handle = async function handleWithTeacher(req, res, url) {
     if (await teacherFastRead.handle(req, res, url)) return true;
+    if (await teacherFinalizeGuard.handle(req, res, url)) return true;
     if (await teacherHomeworkRelaxed.handle(req, res, url)) return true;
     if (await teacherBatch.handle(req, res, url)) return true;
     if (await teacherV3.handle(req, res, url)) return true;
